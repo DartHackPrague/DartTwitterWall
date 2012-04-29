d8ac01712f0b165e634f3d1dd80c6690c7d3d74e
Filip Hracek <filiph@google.com>
2012-04-29 15:41:15 +0200

    human readable time ago now works

--- TwitterApi.dart
+++ TwitterApi.dart
@@ -47,6 +47,10 @@ class TwitterApi {
         return;
       }
       Map<String,Dynamic> output = JSON.parse( json );
+      if (output == null) {
+        completer.complete([]);
+        return;
+      }
       List<Map<String,Dynamic>> results = output["results"];
 
       List<Tweet> tweets = new List<Tweet>();
@@ -70,7 +74,9 @@ class TwitterApi {
 
       _stringInputStream.onLine = () {
         String stream = _stringInputStream.readLine();
-        completer.complete(stream);
+        if (!completer.future.isComplete) {
+          completer.complete(stream);
+        }
         response.inputStream.close();
       };
     };
@@ -80,9 +86,40 @@ class TwitterApi {
 }
 
 String timeAgoFromCreatedAt(String createdAt) {
-  //RegExp dateMatch = new RegExp();
+  // Sun, 29 Apr 2012 09:42:51 +0000
+  List<String> monthLookup = const ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  RegExp dateMatch = new RegExp(@"(\w+), (\d+) (\w+) (\d+) (\d\d):(\d\d):(\d\d) ((\+|\-)\d\d\d\d)");
+
+  Match m = dateMatch.firstMatch(createdAt);
+  int day = Math.parseInt(m.group(2));
+  String monthStr = m.group(3);
+  int month = monthLookup.indexOf(monthStr) + 1;
+  int year = Math.parseInt(m.group(4));
+  int hour = Math.parseInt(m.group(5));
+  int minute = Math.parseInt(m.group(6));
+  int second = Math.parseInt(m.group(7));
+
+  Date now = new Date.now();
+  Date date = new Date.withTimeZone(year, month, day, hour, minute, second, 0, new TimeZone.utc());
+  Duration diff = now.difference(date);
+
+  if (diff.inDays > 0) {
+    return "${diff.inDays} days ago";
+  } else if (diff.inHours > 1) {
+    return "${diff.inHours} hours ago";
+  } else if (diff.inHours > 0) {
+    return "1 hour and ${diff.inMinutes} minutes ago";
+  } else if (diff.inMinutes > 0) {
+    return "${diff.inMinutes} minutes ago";
+  } else if (diff.inSeconds > 10) {
+    return "${diff.inSeconds} seconds ago";
+  } else {
+    return "just now";
+  }
+
+  return "$date";
   //Date date = new Date.fromString(createdAt);
   //return date.hours.toString();
 
-  return "not yet implemented";
+  //return "not yet implemented";
 }
