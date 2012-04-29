b01b4c93a9d7bf29099e168fbfbf374f6aa177a7
Radek Simko <radek.simko@gmail.com>
2012-04-29 14:55:21 +0200

    Frontend redesigned

--- DartTwitterWall.dart
+++ DartTwitterWall.dart
@@ -6,20 +6,41 @@
 
 void main() {
   TwitterApi api = new TwitterApi();
-  Timer timer = new Timer.repeating( 250, (Timer timer) {
+  int tweetHeight = 4;
+  int terminalWidth = 139;
+  int terminalHeight = 20;
+  
+  Timer timer = new Timer.repeating( 1000, (Timer timer) {
     api.getTweets().then( ( List<Tweet> tweets ) {
+      printHeader();
+      print( 'NOW: ' + new Date.now() + "\n" );
+      
       for ( Tweet tweet in tweets ) {
         print( tweet.text );
+        print( '-- \n from ' + tweet.userName + ' at ' + tweet.timeAgo );
+        print( '\n' );
       }
+      
+      print( repeat( "\n", terminalHeight - tweetHeight * tweets.length ) );
     });
-    
-    print( new Date.now() );
-    repeat( "\n", 12 );
   });
 }
 
-void repeat( String char, int lines ) {
+void printHeader() {
+  print( @"""  
+________                 __ ___________       .__  __    __                __      __        .__  .__   
+\______ \ _____ ________/  |\__    ___/_  _  _|__|/  |__/  |_  ___________/  \    /  \_____  |  | |  |  
+ |    |  \\__  \\_  __ \   __\|    |  \ \/ \/ /  \   __\   __\/ __ \_  __ \   \/\/   /\__  \ |  | |  |  
+ |    `   \/ __ \|  | \/|  |  |    |   \     /|  ||  |  |  | \  ___/|  | \/\        /  / __ \|  |_|  |__
+/_______  (____  /__|   |__|  |____|    \/\_/ |__||__|  |__|  \___  >__|    \__/\  /  (____  /____/____/
+        \/     \/                                                 \/             \/        \/           
+""" );
+}
+
+String repeat( String char, int lines ) {
+  String wholeString = '';
   for(int i = 0; i <= lines; i++) {
-    print( char );
+    wholeString += char;
   }
+  return wholeString;
 }
