fe02e0e6a12dc3da77facee8db3ba8933b87009c
Radek Simko <radek.simko@gmail.com>
2012-04-29 14:21:20 +0200

    API usage changed, repeat func implemented

--- DartTwitterWall.dart
+++ DartTwitterWall.dart
@@ -1,8 +1,26 @@
 #library('twitter_wall');
 
 #import("TwitterApi.dart");
+#import('dart:io');
+#import('dart:core');
 
 void main() {
   TwitterApi api = new TwitterApi();
-  List<Tweet> tweets = api.getTweets(max:5);
+  
+  Timer timer = new Timer.repeating( 250, (Timer timer) {
+    api.getTweets().then( ( List<Tweet> tweets ) {
+      for ( Tweet tweet in tweets ) {
+        print( tweet.text );
+      }
+    });
+    
+    print( new Date.now() );
+    repeat( "\n", 12 );
+  });
+}
+
+void repeat( String char, int lines ) {
+  for(int i = 0; i <= lines; i++) {
+    print( char );
+  }
 }
