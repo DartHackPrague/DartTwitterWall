f77fd86d4612217b0dc12f4c4b39f025802183c4
Radek Simko <radek.simko@gmail.com>
2012-04-29 15:07:11 +0200

    Printing tweets put to extra method

--- DartTwitterWall.dart
+++ DartTwitterWall.dart
@@ -5,24 +5,29 @@
 #import('dart:core');
 
 void main() {
+  printTweets();
+  Timer timer = new Timer.repeating( 5000, (Timer timer) {
+    printTweets();
+  });
+}
+
+void printTweets() {
   TwitterApi api = new TwitterApi();
   int tweetHeight = 4;
   int terminalWidth = 139;
   int terminalHeight = 20;
   
-  Timer timer = new Timer.repeating( 1000, (Timer timer) {
-    api.getTweets().then( ( List<Tweet> tweets ) {
-      printHeader();
-      print( 'NOW: ' + new Date.now() + "\n" );
-      
-      for ( Tweet tweet in tweets ) {
-        print( tweet.text );
-        print( '-- \n from ' + tweet.userName + ' at ' + tweet.timeAgo );
-        print( '\n' );
-      }
-      
-      print( repeat( "\n", terminalHeight - tweetHeight * tweets.length ) );
-    });
+  api.getTweets().then( ( List<Tweet> tweets ) {
+    printHeader();
+    print( 'NOW: ' + new Date.now() + "\n" );
+    
+    for ( Tweet tweet in tweets ) {
+      print( tweet.text );
+      print( '-- \n from ' + tweet.userName + ' at ' + tweet.timeAgo );
+      print( '\n' );
+    }
+    
+    print( repeat( "\n", terminalHeight - tweetHeight * tweets.length ) );
   });
 }
 
